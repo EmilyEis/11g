a5894a9c32dfe25fa13172e39f5d461ee574c2ac
EmilyEis <emily.eeisenberg@gmail.com>
2022-01-07 12:16:11 +0100

    Co-authored-by: ElisaLiberski <ElisaLiberski@users.noreply.github.com>

--- robots-game.fsx
+++ robots-game.fsx
@@ -1,7 +1,16 @@
 // Game
 open Robots
 
-let b1 = BoardDisplay (4,7)
+let start = 
+    let game = Game (Board (4,7))
+    game.Play ()
+
+start
+
+
+
+
+//let b1 = BoardDisplay (4,7)
 //let bb = b1.arrSet
 
 //b1.Set (1,1, "BB")
@@ -13,28 +22,37 @@ let b1 = BoardDisplay (4,7)
 //b1.SetBottomWall (2,3)
 //b1.SetRightWall (2,3)
 
-printfn ""
-let board1 = BoardFrame (4,7)
-let g1 = Goal (3, 6)
-let r1 = Robot (2, 3, "AA")
-let r2 = Robot (1, 1, "BB")
-let r3 = Robot (4, 7, "CC")
-let v1 = VerticalWall (2, 3, 0)
-let h1 = HorizontalWall (2, 3, 0)
-let m1 = Board b1
-m1.AddElement r1
-board1.RenderOn b1
-//r1.RenderOn b1
-r2.RenderOn b1
-r3.RenderOn b1
-g1.RenderOn b1
-v1.RenderOn b1
-h1.RenderOn b1
-b1.Show ()
+// printfn ""
+// let board1 = BoardFrame (4,7)
+// let g1 = Goal (3, 6)
+// let r1 = Robot (2, 3, "AA")
+// let r2 = Robot (1, 1, "BB")
+// let r3 = Robot (4, 7, "CC")
+// let v1 = VerticalWall (2, 3, 0)
+// let h1 = HorizontalWall (2, 3, 0)
+// let m1 = Board b1
+// m1.AddElement r1
+// //board1.RenderOn b1
+// //r1.RenderOn b1
+// //r2.RenderOn b1
+// //r3.RenderOn b1
+// m1.AddRobot r1
+// m1.AddRobot r2
+// m1.AddRobot r3
+// m1.AddElement g1
+// m1.AddElement v1
+// m1.AddElement h1
+// // g1.RenderOn b1
+// // v1.RenderOn b1
+// // h1.RenderOn b1
+// //List.iter (fun (x:Robot) -> printfn "pos: %A; name: %s" x.Position x.Name) m1.Robots
+// //List.iter (fun (x:BoardElement) -> printfn "%A" x) m1.Elements
+
+// b1.Show ()
 
 
-let h = HorizontalWall (2, 3, 2)
-let d = BoardDisplay (5, 5)
+// let h = HorizontalWall (2, 3, 2)
+// let d = BoardDisplay (5, 5)
 
 
 //printfn "%A" (h.RenderOn d) // shows nothing right now
--- robots.fs
+++ robots.fs
@@ -1,7 +1,6 @@
 // 11g0
 module Robots
 
-// jeg har ændret her bare så jeg kunne få lov til at arbejde videre med det
 type BoardDisplay (rows:int, cols:int) =
     let positions = Array.allPairs [|1..rows|] [|1..cols|]
     let arrS = [|for n in positions do n, "  "|]
@@ -58,14 +57,11 @@ and Robot (row:int, col:int, name:string) =
         | South -> if c1-1 = c2 then Stop (r1, c1) else Continue (dir, (r1, c1))
         | East  -> if r1+1 = r2 then Stop (r1, c1) else Continue (dir, (r1, c1))
         | West  -> if r1-1 = r2 then Stop (r1, c1) else Continue (dir, (r1, c1))
-        
     override this.RenderOn display =    // ok
         display.Set (fst this.Position, snd this.Position, this.Name)
-
     member val Name = name      // ok
-
-    member robot.Step dir =     // ok
-        let (r,c) = robot.Position
+    member this.Step dir =     // ok
+        let (r,c) = this.Position
         match dir with
         | North -> pos <- (r, c+1)
         | South -> pos <- (r, c-1)
@@ -82,33 +78,78 @@ and Goal (r:int, c:int) =
 // Sets an inner vertical wall in a board
 and VerticalWall (r:int, c:int, n:int) =   // ok
     inherit BoardElement ()
+    override this.Interact other dir = 
+        let (r1, c1) = other.Position
+        let llst = [for i in 0..n do (r, c+i)]
+        match dir with 
+        | North -> if List.contains (r1, c1+1) llst then Stop (r1, c1+1) else Continue (dir, (r1, c1))        
+        | South -> if List.contains (r1, c1-1) llst then Stop (r1, c1-1) else Continue (dir, (r1, c1))
+        | _ -> Ignore
     override this.RenderOn display = 
         for i in 0..n do display.SetRightWall ((if n > 0 then r+i else r-i), c)
 
 // Sets an inner horizontal wall in a board
-and HorizontalWall (r:int, c:int, n:int) =     // ok
+and HorizontalWall (r:int, c:int, n:int) = 
     inherit BoardElement ()
+    override this.Interact other dir =
+        let (r1, c1)= other.Position
+        let llst = [for i in 0..n do (r+i, c)]
+        match dir with
+        | East -> if List.contains (r1+1, c1) llst then Stop (r1+1, c1) else Continue (dir, (r1, c1))
+        | West -> if List.contains (r1-1, c1) llst then Stop (r1-1, c1) else Continue (dir, (r1, c1))
+        | _ -> Ignore
     override this.RenderOn display = 
         for i in 0..n do display.SetBottomWall (r, if n > 0 then c+i else c-i)
 
 // Not sure what it is supposed to do
 and BoardFrame (r:int, c:int) =
     inherit BoardElement ()
-    override this.RenderOn display = display.SetBottomWall (r, c); display.SetRightWall (r, c)
+    override this.Interact other dir =
+        let (r1, c1) = other.Position
+        let llst = List.allPairs [1..r] [1..c]
+        match dir with
+        | North -> if List.contains (r1, c1+1) llst then Stop (r1, c1+1) else Continue (dir, (r1, c1))        
+        | South -> if List.contains (r1, c1-1) llst then Stop (r1, c1-1) else Continue (dir, (r1, c1))
+        | East  -> if List.contains (r1+1, c1) llst then Stop (r1+1, c1) else Continue (dir, (r1, c1))
+        | West  -> if List.contains (r1-1, c1) llst then Stop (r1-1, c1) else Continue (dir, (r1, c1))
+    override this.RenderOn display = 
+        for i in 0..c-1 do display.Set (r, i, "--+")
+        for n in 0..r-1 do display.Set (n, c, "|")
+        //display.SetBottomWall (r, c); display.SetRightWall (r, c)  // ???
 
-type Board (display: BoardDisplay) =
-    member this.AddRobot (robot:Robot) = robot.RenderOn display
-    member this.AddElement (element:BoardElement) = element.RenderOn display
-    member this.Elements = []    // returns BoardElement list
-    member this.Robots = []      // returns Robot list
+type Board (rows:int, cols:int) =
+    let mutable elementList = []
+    let mutable robotList = []
+    let dis = BoardDisplay (rows, cols)
+    member this.display = dis
+    member this.AddRobot (robot:Robot) =    
+        robot.RenderOn this.display
+        robotList <- robot::robotList
+        //elementList <- robot::elementList
+    member this.AddElement (element:BoardElement) =     
+        element.RenderOn this.display
+        elementList <- element::elementList
+    member this.Elements = elementList
+    member this.Robots = robotList
     member this.Move (robot:Robot, dir:Direction) = 
         for x in this.Robots do
             if not (robot = x) then
                 match robot.Interact x dir with
                 | Continue (dir, (r, c)) -> robot.Step (dir)
-                | Stop (r, c)            -> robot.Position <- (r, c)
+                | Stop (r, c)            -> robot.Position <- (r, c); robot.RenderOn this.display
                 | Ignore                 -> robot.Step (dir)
 
 
-        // while List.exists (fun (x:Robot) -> robot.Interact x dir = Continue (_,_,_)) robots do
-        //     List.map (fun (x:Robot) -> robot.Interact x dir) robots
+type Game (board: Board) =
+    let mutable moves = 0
+    member this.Play () =  // return number of moves before game over
+        System.Console.Clear ()
+        printfn "------------ Let's play Ricochet Robots! ------------\n"
+        board.AddElement (BoardFrame (4,7))
+        let AA = Robot (1,1,"AA")
+        board.AddRobot AA
+        board.Move (AA, East)
+        printfn "\nThis is your board!"
+        board.display.Show ()
+        
+        
